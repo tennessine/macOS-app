d8825bda885c4f039c21c66b8d14ade70814f146
gekongfei <389443626@qq.com>
2018-06-24 12:07:58 +0800

    Initial Commit

---
+++ Demo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by gekongfei on 2018/6/24.
+//  Copyright © 2018 gekongfei. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
